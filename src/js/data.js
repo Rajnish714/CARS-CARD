e1ce2896916fd7cfd3bf1e3b9741ab1c2514c7fa
Rajnish714 <srajnish714@gmail.com>
2023-07-10 02:53:20 +0530

    Card Card updated

--- src/js/data.js
+++ src/js/data.js
@@ -1,16 +1,17 @@
-// data.js
+
+// data1
 export const data1 = [
-    { id: 1, title: 'Company 1', description: 'Description for Company 1' },
-    { id: 2, title: 'Company 2', description: 'Description for Company 2' },
-    { id: 3, title: 'Company 3', description: 'Description for Company 3' }
+    { id: 1, title: 'TATA', description: 'Tata Motors Limited is an Indian multinational automotive manufacturing company.' },
+    { id: 2, title: 'MARUTI SUZUKI', description: 'Maruti Suzuki India Limited is an Indian automobile manufacturer.' },
+    { id: 3, title: 'HYUNDAI', description: 'Hyundai Motor Company is a South Korean multinational automotive manufacturer.' }
 ];
 
+// data2
 export const data2 = [
-    { id: 1, companyId: 1, title: 'Car Model 6', description: 'Description for Car Model 6' },
-    { id: 2, companyId: 1, title: 'Car Model 5', description: 'Description for Car Model 5' },
-    { id: 3, companyId: 2, title: 'Car Model 4', description: 'Description for Car Model 4' },
-    { id: 4, companyId: 2, title: 'Car Model 3', description: 'Description for Car Model 3' },
-    { id: 5, companyId: 3, title: 'Car Model 2', description: 'Description for Car Model 2' },
-    { id: 6, companyId: 3, title: 'Car Model 1', description: 'Description for Car Model 1' },
-
+    { id: 1, companyId: 1, title: 'TATA Tiago', description: 'The Tata Tiago is a popular hatchback known for its stylish design and comfortable interiors. It offers great fuel efficiency and comes equipped with advanced features.' },
+    { id: 2, companyId: 1, title: 'TATA Nexon', description: 'The Tata Nexon is a compact SUV that combines performance, safety, and style. It offers spacious interiors, advanced safety features, and a powerful engine.' },
+    { id: 3, companyId: 2, title: 'MARUTI SUZUKI Swift', description: 'The Maruti Suzuki Swift is a versatile hatchback known for its sporty design and excellent performance. It offers a comfortable ride, fuel efficiency, and modern features.' },
+    { id: 4, companyId: 2, title: 'MARUTI SUZUKI Baleno', description: 'The Maruti Suzuki Baleno is a premium hatchback that offers a perfect blend of style, comfort, and performance. It comes with spacious interiors, advanced safety features, and a host of modern technologies.' },
+    { id: 5, companyId: 3, title: 'HYUNDAI Creta', description: 'The Hyundai Creta is a popular compact SUV known for its premium features and impressive performance. It offers a spacious cabin, advanced infotainment system, and a comfortable driving experience.' },
+    { id: 6, companyId: 3, title: 'HYUNDAI i20', description: 'The Hyundai i20 is a stylish and feature-packed premium hatchback. It offers a modern design, comfortable interiors, advanced safety features, and a range of engine options.' }
 ];
